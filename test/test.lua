477dc14119d4f5d59d5e2c7e489bbd987c5aec67
vinoca <vinoca@vinoca.org>
2019-10-23 16:43:04 +0800

    fix color format

--- test/test.lua
+++ test/test.lua
@@ -5,7 +5,6 @@ qr = require("qrencode")
 -- print(qr:encode("is ok?"))
 -- print(qr("is ok?"))
 
---[[
 print(qr {
     text="is ok?",
     level="L",
@@ -15,10 +14,9 @@ print(qr {
     symversion=0,
     dpi=78,
     casesensitive=false,
-    foreground="#48AF6D",
-    background="#3FAF6F"
+    foreground="48AF6D",
+    background="3FAF6F"
 }
 )
-]]--
 
-print(qr {text = "is ok(無問題)?", level = "M"})
+--print(qr {text = "is ok(無問題)?", level = "M"})
